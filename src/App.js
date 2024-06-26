3565227bede0d6cb9d5ee2a581fca285a35ce36a
ichandrasharma <chandrasharma36912@gmail.com>
2024-06-26 16:30:56 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from "./components/Navbar";
+import Form from "./components/Form";
+import React, { useState } from "react";
+import Alert from "./components/Alert";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showsAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1000);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#52045c";
+      showsAlert("Dark Mode Enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showsAlert("Light Mode Enabled", "success");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div>
+        <Navbar title="TextManipulator" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <Form showsAlert={showsAlert} heading="Analyse Your Text" mode={mode} />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Form.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+
+export default function Form(props) {
+  const UPclick = () => {
+    let newtext = text.toUpperCase();
+    setText(newtext);
+    props.showsAlert("Converted to UPPERCASE!", "success");
+  };
+
+  const loclick = () => {
+    let newtext = text.toLowerCase();
+    setText(newtext);
+    props.showsAlert("Converted to lowercase!", "success");
+  };
+  
+  const delclick = () => {
+    let newtext = "";
+    setText(newtext);
+    props.showsAlert("Text Deleted!!!", "success");
+  };
+  
+  const copyclick = () => {
+    let newtext = document.querySelector("#box");
+    newtext.select();
+    navigator.clipboard.writeText(newtext.value);
+    props.showsAlert("Copied to clipboard!", "success");
+  };
+  
+  const spaceclick = () => {
+    let newtext = text.split(/[ ]+/);
+    setText(newtext.join(" "));
+    props.showsAlert("Extra Spaces Removed!!!", "success");
+  };
+
+  const [text, setText] = useState("");
+
+  const handleOnchng = (evt) => {
+    setText(evt.target.value);
+  };
+
+  return (
+    <>
+      <div className="container" style = {{color: props.mode === "dark" ? "white" : "#52045c"}}>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            id="box"
+            rows="8"
+            value={text}
+            onChange={handleOnchng}
+            style = {{backgroundColor: props.mode === "dark" ? "#a47cc8" : "white", color: props.mode === "dark" ? "white" : "#52045c"}}
+          ></textarea>
+        </div>
+        <div>
+          <button className="btn btn-primary mx-2" onClick={UPclick}>
+            Turn into UPPERCASE
+          </button>
+          <button className="btn btn-primary mx-2" onClick={loclick}>
+            Turn into lowercase
+          </button>
+          <button className="btn btn-primary mx-2" onClick={delclick}>
+            Delete All
+          </button>
+          <button className="btn btn-primary mx-2" onClick={copyclick}>
+            Copy All
+          </button>
+          <button className="btn btn-primary mx-2" onClick={spaceclick}>
+            Remove Extra Spaces
+          </button>
+        </div>
+        <div className="container my-3" style={{color: props.mode === "dark" ? "white" : "#52045c"}}>
+          <h2>Full Summary</h2>
+          <p>
+            {text.split("").length} words and {text.length} characters
+          </p>
+          <p>{0.008 * text.split("").length} minutes read</p>
+          <h2>Preview</h2>
+          <p>{text.length > 0 ? text: "Nothing Here To check Please type above box"}</p>
+        </div>
+      </div>
+    </>
+  );
+}
